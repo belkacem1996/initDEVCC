913dd65dce0c2aa87cc3ac1e2bdd862bc56cfe63
belkacem1996 <mohaliclay1996@gmail.com>
2018-04-05 20:19:25 +0100

    init

---
+++ initdev/intdev-help.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+echo "name : ' nom de projet ' is application"
+echo "syntax : initdev"
+echo "author : zouilekh belkacem , belkacemyesse@gmakil.com"
+echo "args : -GPL:generale public license ,-mit!license mit, -c:proget en language c, -cpp: projet en language cpp, -py: projet en language python, -latex: projet de reduction een latex, -beamer: projet de presentation en beamer"
+
+
